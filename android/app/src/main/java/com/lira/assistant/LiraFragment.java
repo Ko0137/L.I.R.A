1e86de332596acaea869dbd990fe2a260bb4795a
Ko0137 <droop.fix.droop@gmail.com>
2026-09-21 01:02:26 +0500

    feat: add ContextCompat import to LiraFragment

--- android/app/src/main/java/com/lira/assistant/LiraFragment.java
+++ android/app/src/main/java/com/lira/assistant/LiraFragment.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
